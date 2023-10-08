9bd3d7be77ea331b3b9b249087b5337597b8a55f
KALTURA\oleg.sigalov <oleg.sigalov@kaltura.com>
2023-10-08 15:51:03 +0300

    added logo, and logic files

---
+++ core/base_practitest.py
@@ -0,0 +1,235 @@
+import logging
+import time
+from enum import Enum
+import json
+import static_methods
+from aws_sqs_pusher import SQSPusher
+
+
+class BasePractiTest:
+    WAIT_EXPONENTIAL_MULTIPLIER = 10000
+    WAIT_EXPONENTIAL_MAX = 60000
+
+    def __init__(self, pt_username,
+                 filter_id_list,
+                 pt_token,
+                 access_key,
+                 secret_key,
+                 project_name,
+                 practitest_project_id,
+                 practitest_trigger_filter_id=None,
+                 practitest_execute_automated=None,
+                 practitest_automation_run_only=None,
+                 processed_field_id=None,
+                 processed_field_value=None,
+                 practitest_automation_trigger=None,
+                 practitest_automation_trigger_value=None,
+                 practitest_aws_instance_type=None,
+                 practitest_debug=None,
+                 execution_type=None,
+                 sync_exec=None
+                 ):
+        # PracitTest fields ID
+        self.PRACTITEST_USER_NAME = pt_username
+        self.PRACTITEST_TRIGGER_FILTER_ID_LIST = filter_id_list
+        self.PRACTITEST_API_TOKEN = pt_token
+        self.AWS_ACCESS_KEY = access_key
+        self.AWS_SECRET_KEY = secret_key
+        self.PROJECT_NAME = project_name
+        self.PRACTITEST_PROJECT_ID = practitest_project_id
+        self.PRACTITEST_EXECUTE_AUTOMATED = practitest_execute_automated
+        self.PRACTITEST_AUTOMATION_RUN_ONLY = practitest_automation_run_only
+        self.PROCESSED_FIELD_ID = processed_field_id
+        self.PROCESSED_FIELD_VALUE = processed_field_value
+        self.PRACTITEST_TRIGGER_FILTER_ID = practitest_trigger_filter_id
+        self.PRACTITEST_AWS_INSTANCE_TYPE = practitest_aws_instance_type
+        self.PRACTITEST_DEBUG = practitest_debug
+        self.EXECUTION_TYPE = execution_type
+        self.SYNCHRONOUS_EXECUTION = sync_exec
+
+        # Trigger fields
+        self.PRACTITEST_AUTOMATION_TRIGGER = practitest_automation_trigger
+        self.PRACTITEST_AUTOMATION_TRIGGER_VALUE = practitest_automation_trigger_value
+
+        # PractiTest API URLs
+        self.BASE_URL = "https://api.practitest.com/api/v2/projects/" + self.PRACTITEST_PROJECT_ID
+        self.RUNS_URI = f'{self.BASE_URL}/runs.json?developer_email={self.PRACTITEST_USER_NAME}&api_token={self.PRACTITEST_API_TOKEN}'
+        self.INSTANCE_URI = f'{self.BASE_URL}/instances.json?developer_email={self.PRACTITEST_USER_NAME}&api_token={self.PRACTITEST_API_TOKEN}'
+        self.SETS_URI = f'{self.BASE_URL}/sets.json?developer_email={self.PRACTITEST_USER_NAME}&api_token={self.PRACTITEST_API_TOKEN}'
+        self.SPECIFIC_SET_URI = f'{self.BASE_URL}/sets/YOUR_SET_ID.json?developer_email={self.PRACTITEST_USER_NAME}&api_token={self.PRACTITEST_API_TOKEN}'
+        self.CLONE_TEST_SET = f'{self.BASE_URL}/sets/YOUR_SET_ID/clone.json?developer_email={self.PRACTITEST_USER_NAME}&api_token={self.PRACTITEST_API_TOKEN}'
+        self.HEADERS = {
+            'Content-Type': 'application/json',
+            'Connection': 'close'
+        }
+
+    class TestStatusEnum(Enum):
+        def __str__(self):
+            return str(self.value)
+
+        PASSED = 'PASSED'
+        FAILED = 'FAILED'
+        BLOCKED = 'BLOCKED'
+        NO_RUN = 'NO RUN'
+        N_A = 'N/A'
+
+    def create_tests_json(self, filter_test_sets):
+        tests_dict = [] #Contains all the tests to be executed (pushed to queue)
+        if not filter_test_sets:
+            logging.info(f'Warning: No test set found under {self.PRACTITEST_TRIGGER_FILTER_ID} filter, but should be found')
+            return
+        for test_set in filter_test_sets:
+            try:
+                test_set_attributes = test_set['attributes']
+                test_set_custom_fields = test_set['attributes']['custom-fields']
+                tests = self.get_list_of_tests_by_status(test_set, 'all')
+                for test in tests:
+                    test_dict = {}
+                    test_attributes = test['attributes']
+                    test_custom_fields = test['attributes']['custom-fields']
+                    test_dict['project_name'] = self.PROJECT_NAME
+                    test_dict['test_id'] = str(test_attributes['test-display-id'])
+                    test_dict['test_instance'] = str(test['id']) #Unique test instance id, reporting back to PractiTest
+                    test_dict['test_name'] = str(test_attributes['name']).replace("'", "").replace('"', '').replace(',', '')\
+                        .replace('(', '').replace(')', '').replace('<', '').replace('>', '').replace('!', '').replace('@', '')\
+                        .replace('#', '').replace('*', '')
+                    test_dict['test_set_id'] = str(test_set_attributes['display-id'])
+                    test_dict['test_set_name'] = str(test_set_attributes['name'])
+                    test_dict['project_pt_id'] = str(test_attributes['project-id'])
+                    test_dict['execution_session_id'] = str(time.time()).replace(".","") #Timestamp for current execution, relevant for sync execution
+                    test_dict['automation_run_only'] = str(test_set_custom_fields[self.PRACTITEST_AUTOMATION_RUN_ONLY])
+                    test_dict['aws_instance_type'] = str(test_set_custom_fields[self.PRACTITEST_AWS_INSTANCE_TYPE])
+                    test_dict['debug'] = self.get_prioritized_value(self.PRACTITEST_DEBUG, test_set, test, is_boolean=True)
+                    test_dict['execution_type'] = str(self.EXECUTION_TYPE)
+                    test_dict['sync_exec'] = static_methods.try_to_get_from_dict(test_set_custom_fields, self.SYNCHRONOUS_EXECUTION, is_boolean=True)
+                    tests_dict.append(test_dict)
+            except:
+                logging.info(f'Error: failed to parse test set/ test attributes')
+        return tests_dict
+
+    def get_prioritized_value(self, dict_value, test_set, test, is_boolean=False):
+        """
+        Retrieve a prioritized value from either 'test', 'testset', or 'default' keys.
+
+        This function tries to extract a value based on conditions specified in the input dict_value.
+        It first checks the 'test' key, then 'testset', and finally the 'default' key. If none
+        of these keys provide a value, an exception is raised.
+
+        Parameters:
+        - dict_value (dict): Dictionary containing keys ('test', 'testset', 'default') to specify the desired value.
+        - test_set (dict): Dictionary containing the attributes of the test set.
+        - test (dict): Dictionary containing the attributes of the test.
+        - is_boolean: If true, it will convert 'yes'/'no' to 'true'/'false' string
+
+        Returns:
+        - value: The extracted value based on the conditions in dict_value.
+
+        Raises:
+        - ValueError: If the desired value cannot be retrieved.
+        """
+
+        test_set_attributes = test_set['attributes']
+        test_set_custom_fields = test_set_attributes['custom-fields']
+        test_attributes = test['attributes']
+        test_custom_fields = test_attributes['custom-fields']
+
+        # Helper function to extract value based on field name and source data
+        def extract_value(field, attributes, custom_fields):
+            if '---f-' in field:
+                val = custom_fields[field]
+            else:
+                val = attributes[field]
+
+            # Handle boolean conversion
+            if is_boolean:
+                if val == 'yes':
+                    return 'true'
+                elif val == 'no':
+                    return 'false'
+            return val
+
+        try:
+            # Check for 'test' key
+            if 'test' in dict_value:
+                return str(extract_value(dict_value['test'], test_attributes, test_custom_fields))
+        except:
+            pass
+        try:
+            # Check for 'testset' key
+            if 'testset' in dict_value:
+                return str(extract_value(dict_value['testset'], test_set_attributes, test_set_custom_fields))
+        except:
+            pass
+
+        # Check for 'default' key
+        if 'default' in dict_value:
+            return str(dict_value['default'])
+
+        # If none of the above conditions are met, raise an exception
+        raise ValueError("Could not retrieve the desired field value.")
+
+    def get_list_of_tests_by_status(self, test_set_id, status):
+        """Return all test sets under specific filter id
+        :param test_set_id: test set id as int or string
+        :param status: TestStatusEnum: PASSED, FAILED, BLOCKED, NO_RUN, N_A, ALL
+        :return: dictionary
+        """
+        tests_to_execute = []
+        page = 1
+        while True:
+            url = self.INSTANCE_URI + "&set-ids=" + str(test_set_id['id']) + "&page[number]=" + str(page)
+            # For next iteration
+            page = page + 1
+            response = static_methods.wait_for_request_200('get', url, self.HEADERS,
+                                                 f'Bad response for get_list_of_tests_by_status; Going to retry')
+            dct_sets = json.loads(response.text)
+            if len(dct_sets["data"]) > 0:
+                for test_instance in dct_sets["data"]:
+                    test_instance_atrr = test_instance['attributes']
+                    # If status is 'ALL', will add the test with any status
+                    if status.lower() == 'all':
+                        tests_to_execute.append(test_instance)
+                    # Get only if the test matches to given status
+                    elif test_instance_atrr['run-status'].lower() == status.lower():
+                        tests_to_execute.append(test_instance)
+            else:
+                if page <= 2:
+                    logging.info("No instances in set. " + response.text)
+                break
+                # if page > 2:
+                #     logging.info("Finished getting tests from test set: '" + test_set_id['attributes']['name'] +
+                #           "' ID: " + str(test_set_id['attributes']['display-id']))
+                # else:
+                #     logging.info("No instances in set. " + response.text)
+                # break
+        return tests_to_execute
+
+    def get_all_testsets_under_specific_filter_id(self, filter_id):
+        """Return all test sets under specific filter id
+        :param filter_id: filter id as int ot string
+        :return: dictionary
+        """
+        url = self.SETS_URI + "&filter-id=" + str(filter_id)
+        response = static_methods.wait_for_request_200('get', url, self.HEADERS,
+                                             f'Bad response for get_all_testsets_under_specific_filter_id; Going to retry')
+        return static_methods.get_dict_data_if_not_empty(json.loads(response.text))
+
+    def get_count_of_test_sets_under_filter(self, filter_id):
+        return static_methods.safe_len(self.get_all_testsets_under_specific_filter_id(filter_id))
+
+    def is_to_trigger(self):
+        """Return True if there are test sets under specific filter (PRACTITEST_TRIGGER_FILTER_ID) from json
+        :return: True or False
+        """
+        if self.get_count_of_test_sets_under_filter(self.PRACTITEST_TRIGGER_FILTER_ID) > 0:
+            return True
+        else:
+            return False
+
+    def push_to_sqs(self, tests_to_execute, debug=True):
+        if debug:
+            static_methods.write_dict_to_json_file(tests_to_execute, 'tests_to_execute.json')
+            return
+        sqs_pusher = SQSPusher(access_key=self.AWS_ACCESS_KEY, secret_key=self.AWS_SECRET_KEY)
+        sqs_pusher.push_to_queue()
+        return
---
+++ core/kms/kms_practitest.py
@@ -0,0 +1,76 @@
+import logging
+from time import sleep
+
+from aws_infra.base_practitest import BasePractiTest
+from static_methods import try_to_get_from_dict
+
+
+class KmsPractiTest(BasePractiTest):
+    def __init__(self, more_data=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Additional initialization for KmsPractiTest
+        self.ENVIRONMENT = more_data['environment']
+        self.BROWSER = more_data['browser']
+        self.UPDATE_HOST_FILE = more_data['update_host_file']
+        self.EXECUTE_AT_NIGHT = more_data['execute_at_night']
+        self.VERIFY_VERSIONS = more_data['verify_versions']
+        self.KMS_BUILD = more_data['kmsBuild']
+        self.PLAYER_VERSION = more_data['playerVersion']
+        self.ASSIGNED_TO = more_data['assigned_to']
+        self.AUTOMATION_ARGUMENTS = more_data['automation_arguments']
+        self.PARTNER = more_data['partner']
+        self.BASE_URL = more_data['base_url']
+        self.ADMIN_USERNAME = more_data['admin_username']
+        self.ADMIN_PASSWORD = more_data['admin_password']
+        self.LOGIN_USERNAME = more_data['login_username']
+        self.LOGIN_PASSWORD = more_data['login_password']
+        self.PLAYER_VERSION_V7 = more_data['playerVersionV7']
+
+    # Add any methods specific to KmsPractiTest here
+    def start_service(self):
+        if super().is_to_trigger():
+            tests_to_execute = self.trigger_execution()
+            self.push_to_sqs(tests_to_execute)
+            logging.info('DEBUG: Done')
+            sleep(10)
+        else:
+            logging.info("no sets found under filter id")
+
+    def trigger_execution(self):
+        try:
+            logging.info("Execution Triggered")
+            filter_test_sets = self.get_all_testsets_under_specific_filter_id(self.PRACTITEST_TRIGGER_FILTER_ID)  # TODO make multiple filter - list
+            initial_tests_list = super().create_tests_json(filter_test_sets)
+            if not initial_tests_list:
+                raise
+            for test_set in filter_test_sets:
+                try:
+                    test_set_attributes = test_set['attributes']
+                    test_set_custom_fields = test_set['attributes']['custom-fields']
+                    tests = self.get_list_of_tests_by_status(test_set, 'all')
+                    for test in tests:
+                        test_attributes = test['attributes']
+                        test_custom_fields = test['attributes']['custom-fields']
+                        for initial_test in initial_tests_list:
+                            if str(initial_test['test_id']) == str(test_attributes['test-display-id']):
+                                initial_test['environment'] = try_to_get_from_dict(test_set_custom_fields, self.ENVIRONMENT)
+                                initial_test['browser'] = self.get_prioritized_value(self.BROWSER, test_set, test, is_boolean=True)
+                                initial_test['update_host_file'] = try_to_get_from_dict(test_set_custom_fields, self.UPDATE_HOST_FILE, is_boolean=True)
+                                initial_test['execute_at_night'] = try_to_get_from_dict(test_set_custom_fields, self.EXECUTE_AT_NIGHT, is_boolean=True)
+                                initial_test['verify_versions'] = try_to_get_from_dict(test_set_custom_fields,self.VERIFY_VERSIONS, is_boolean=True)
+                                initial_test['kmsBuild'] = try_to_get_from_dict(test_set_custom_fields, self.KMS_BUILD)
+                                initial_test['playerVersion'] = try_to_get_from_dict(test_set_custom_fields, self.PLAYER_VERSION)
+                                initial_test['assigned_to'] = try_to_get_from_dict(test_set_attributes ,self.ASSIGNED_TO)
+                                initial_test['automation_arguments'] = try_to_get_from_dict(test_set_custom_fields, self.AUTOMATION_ARGUMENTS, default_value='')
+                                initial_test['partner'] = try_to_get_from_dict(test_set_custom_fields, self.PARTNER, default_value='')
+                                initial_test['base_url'] = try_to_get_from_dict(test_set_custom_fields, self.BASE_URL, default_value='')
+                                initial_test['admin_username'] =  try_to_get_from_dict(test_custom_fields, self.ADMIN_USERNAME, default_value='')
+                                initial_test['admin_password'] =  try_to_get_from_dict(test_custom_fields, self.ADMIN_PASSWORD, default_value='')
+                                initial_test['login_username'] =  try_to_get_from_dict(test_custom_fields, self.LOGIN_USERNAME, default_value='')
+                                initial_test['login_password'] =  try_to_get_from_dict(test_custom_fields, self.LOGIN_PASSWORD, default_value='')
+                                initial_test['playerVersionV7'] =  try_to_get_from_dict(test_custom_fields, self.PLAYER_VERSION_V7, default_value='')
+                except:
+                    logging.info(f'Error: failed to parse test set/ test attributes')
+        except:
+            logging.info("Error: failed to trigger execution, skipping this execution")
+        return initial_tests_list
